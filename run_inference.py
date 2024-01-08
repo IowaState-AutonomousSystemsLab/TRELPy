6251055c43fd95714c1b4e5e0ad3a2a1e9527861
apurva <abadithe@caltech.edu>
2024-01-08 10:26:37 -0800

    run inference

--- run_inference.py
+++ run_inference.py
@@ -3,28 +3,35 @@ import os
 import subprocess
 from pathlib import Path
 import pdb
+from datetime import datetime
+now = datetime.now()
 
 home_dir = str(Path.home())
-
 setsize = "mini"
+configs_path = "configs/pointpillars/pointpillars_hv_fpn_sbn-all_8xb4-2x_nus-3d.py"
+checkpoint_path = "checkpoints/hv_pointpillars_fpn_sbn-all_4x8_2x_nus-3d_20210826_104936-fca299c1.pth"
+
+folder_name = "model_"+now.strftime("%m-%d-%Y_%H_%M")
 
 if setsize == "mini":
     dataset_name = "nuscenes-mini"
-    out_dir = f"{home_dir}/nuscenes_dataset/inference_results_mini"
+    out_dir = f"{home_dir}/nuscenes_dataset/inference_results_mini/"+folder_name
 else:
     dataset_name = "nuscenes"
-    out_dir = f"{home_dir}/nuscenes_dataset/inference_results"
+    out_dir = f"{home_dir}/nuscenes_dataset/inference_results/"+folder_name
+
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+
+info_file = os.path.join(out_dir, "model_info.txt")
+with open(info_file, 'w') as f:
+    f.write(f"configs_path = {configs_path} \n checkpoint_path = {checkpoint_path} \n")
+f.close()
     
 pcd_path = f"{home_dir}/software/mmdetection3d/data/{dataset_name}/samples/LIDAR_TOP/"
 
 pcd_list = os.listdir(pcd_path)
 print(len(pcd_list))
-
-# configs_path = "configs/pointpillars/pointpillars_hv_fpn_sbn-all_8xb4-2x_nus-3d.py"
-# checkpoint_path = "checkpoints/hv_pointpillars_fpn_sbn-all_4x8_2x_nus-3d_20210826_104936-fca299c1.pth"
-
-configs_path = "configs/pointpillars/pointpillars_hv_fpn_sbn-all_8xb2-amp-2x_nus-3d.py"
-checkpoint_path = "checkpoints/hv_pointpillars_fpn_sbn-all_fp16_2x8_2x_nus-3d_20201021_120719-269f9dd6.pth"
 
 for i, pcd in enumerate(pcd_list):
     path = Path(f"{pcd_path}/{pcd}").absolute()
@@ -36,3 +43,7 @@ for i, pcd in enumerate(pcd_list):
     
     if i%100 == 0:
         print(f"---- ---- !-!-!-!- run_inference.py: Done with {i} files")
+
+with open(info_file, 'a') as f:
+    f.write(f"Inferences complete.")
+f.close()
