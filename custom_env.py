a9ce8b5ccc68c10443f50e47fd608b34738fd496
Ranai <ranais@iastate.edu>
2024-02-05 18:45:01 -0600

    quaternion explanation

--- custom_env.py
+++ custom_env.py
@@ -6,8 +6,19 @@ from pathlib import Path
 
 ######## PARMS #########
 model_name = "model2_good"
+modality = "lidar"
 is_mini = True
 ########################
+
+def create_dir_if_not_exist(dir_path):
+    if not os.path.exists(dir_path):
+        print(f"Directory {dir_path} not found. Creating...")
+        os.makedirs(dir_path)
+    else:
+        print(f"Not creating {dir_path} because it already exists")
+
+def is_set_to_mini():
+    return is_mini
 
 if is_mini:
     dataset = "nuscenes-mini"
@@ -16,12 +27,11 @@ else:
     dataset = "nuscenes"
     inf_res = "inference_results"
 
-
 home_dir = str(Path.home())
 dataset_root = f"{home_dir}/software/mmdetection3d/data/{dataset}/"
 output_dir = f"{home_dir}/nuscenes_dataset/{inf_res}"
 model_dir = str(Path(f"{output_dir}/{model_name}").absolute())
 preds_dir = str(Path(f"{model_dir}/preds").absolute())
-
-def is_set_to_mini():
-    return is_mini
+repo_dir = str(Path(f"{home_dir}/nuscenes_dataset/3D_Detection").absolute())
+cm_dir = str(Path(f"{repo_dir}/saved_cms/{modality}/{dataset}/{model_name}").absolute())
+create_dir_if_not_exist(cm_dir)
--- system_evaluation/experiment_file.py
+++ system_evaluation/experiment_file.py
@@ -1,2 +1,42 @@
-cm_fn = "/home/apurvabadithela/software/run_nuscenes_evaluations/saved_cms/lidar/mini/cm.pkl"
-control_dir = "/home/apurvabadithela/software/run_nuscenes_evaluations/system_evaluation/controllers/"
+# cm_fn = "/home/apurvabadithela/software/run_nuscenes_evaluations/saved_cms/lidar/mini/cm.pkl"
+# control_dir = "/home/apurvabadithela/software/run_nuscenes_evaluations/system_evaluation/controllers/"
+
+import os
+import sys
+from pathlib import Path
+
+######## PARMS #########
+model_name = "model2_good"
+modality = "lidar"
+is_mini = True
+########################
+
+if is_mini:
+    dataset = "nuscenes-mini"
+    inf_res = "inference_results_mini"
+else:
+    dataset = "nuscenes"
+    inf_res = "inference_results"
+
+is_mini = True
+def is_set_to_mini():
+    return is_mini
+
+def create_dir_if_not_exist(dir_path):
+    if not os.path.exists(dir_path):
+        print(f"Directory {dir_path} not found. Creating...")
+        os.makedirs(dir_path)
+    else:
+        print(f"Not creating {dir_path} because it already exists")
+
+
+home_dir = str(Path.home())
+dataset_root = f"{home_dir}/software/mmdetection3d/data/{dataset}/"
+output_dir = f"{home_dir}/nuscenes_dataset/{inf_res}"
+model_dir = str(Path(f"{output_dir}/{model_name}").absolute())
+preds_dir = str(Path(f"{model_dir}/preds").absolute())
+repo_dir = str(Path(f"{home_dir}/nuscenes_dataset/3D_Detection").absolute())
+cm_dir = str(Path(f"{repo_dir}/saved_cms/{modality}/{dataset}/{model_name}").absolute())
+create_dir_if_not_exist(cm_dir)
+cm_fn = f"{cm_dir}/cm.pkl"
+control_dir = f"{repo_dir}/system_evaluation/controllers/"
