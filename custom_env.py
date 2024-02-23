627e6e24439d282cb65260b6a897ca8e430b1c7c
apurva <abadithe@caltech.edu>
2024-02-23 08:01:59 -0800

    custom_env

--- custom_env.py
+++ custom_env.py
@@ -3,12 +3,17 @@
 import os
 import sys
 from pathlib import Path
+import subprocess
 
 ######## PARMS #########
-model_name = "model2_good"
+## ONLY Change model_name here to make it work with your version.
+model_name = "model_01-05-2024_20_22"
 modality = "lidar"
 is_mini = True
 ########################
+#### Get Repo Root #####
+def getGitRoot():
+    return subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
 
 def create_dir_if_not_exist(dir_path):
     if not os.path.exists(dir_path):
@@ -21,10 +26,12 @@ def is_set_to_mini():
     return is_mini
 
 if is_mini:
-    dataset = "nuscenes-mini"
+    dataset = "nuscenes"
+    size = "mini"
     inf_res = "inference_results_mini"
 else:
-    dataset = "nuscenes"
+    dataset = "nuscenes-full"
+    size= "full"
     inf_res = "inference_results"
 
 home_dir = str(Path.home())
@@ -32,6 +39,6 @@ dataset_root = f"{home_dir}/software/mmdetection3d/data/{dataset}/"
 output_dir = f"{home_dir}/nuscenes_dataset/{inf_res}"
 model_dir = str(Path(f"{output_dir}/{model_name}").absolute())
 preds_dir = str(Path(f"{model_dir}/preds").absolute())
-repo_dir = str(Path(f"{home_dir}/nuscenes_dataset/3D_Detection").absolute())
-cm_dir = str(Path(f"{repo_dir}/saved_cms/{modality}/{dataset}/{model_name}").absolute())
+repo_dir = getGitRoot()
+cm_dir = str(Path(f"{repo_dir}/saved_cms/{modality}/{size}/{model_name}").absolute())
 create_dir_if_not_exist(cm_dir)
